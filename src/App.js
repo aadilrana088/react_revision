c4a7905f57ae20c19d32e38a1d9d1f9a067842e8
Mohd Aadil Rana <aadilrana088@gmail.com>
2022-09-01 01:00:06 +0530

    Added routes and final project done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './component/Header';
 import Tasks from './component/Tasks';
 import { useState, useEffect } from 'react';
 import AddTask from './component/AddTask';
+import Footer from './component/Footer';
+import About from './component/About';
 function App() {
     const [showAddTask, setShowAddTask] = useState(false);
     const [tasks, setTasks] = useState([]);
@@ -73,22 +76,37 @@ function App() {
         );
     };
     return (
-        <div className="container">
-            <Header
-                onAdd={() => setShowAddTask(!showAddTask)}
-                showAdd={showAddTask}
-            />
-            {showAddTask && <AddTask onAdd={addTask} />}
-            {tasks.length > 0 ? (
-                <Tasks
-                    tasks={tasks}
-                    onDelete={deleteTask}
-                    onToggle={toggleRemainder}
+        <Router>
+            <div className="container">
+                <Header
+                    onAdd={() => setShowAddTask(!showAddTask)}
+                    showAdd={showAddTask}
                 />
-            ) : (
-                'No task to show'
-            )}
-        </div>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                {showAddTask && <AddTask onAdd={addTask} />}
+                                {tasks.length > 0 ? (
+                                    <Tasks
+                                        tasks={tasks}
+                                        onDelete={deleteTask}
+                                        onToggle={toggleRemainder}
+                                    />
+                                ) : (
+                                    'No task to show'
+                                )}
+                            </>
+                        }
+                    />
+
+                    <Route path="/about" element={<About />} />
+                </Routes>
+
+                <Footer />
+            </div>
+        </Router>
     );
 }
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import Button from './Button';
 const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation();
     return (
         <header className="header">
             <h1>{title}</h1>
-            <Button
-                onClick={onAdd}
-                color={showAdd ? 'red' : 'green'}
-                text={showAdd ? 'Close' : 'Add'}
-            />
+            {location.pathname === '/' && (
+                <Button
+                    onClick={onAdd}
+                    color={showAdd ? 'red' : 'green'}
+                    text={showAdd ? 'Close' : 'Add'}
+                />
+            )}
         </header>
     );
 };
